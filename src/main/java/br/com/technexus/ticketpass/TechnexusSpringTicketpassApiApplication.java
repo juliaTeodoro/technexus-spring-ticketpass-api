96eb19825212a6808f86597d52e2361a23d4f139
Julia Teodoro <julia.teodoro@ipremios.com>
2025-03-29 17:18:53 -0300

    First Commit. Create project!

---
+++ src/main/java/br/com/technexus/ticketpass/TechnexusSpringTicketpassApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.technexus.ticketpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnexusSpringTicketpassApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnexusSpringTicketpassApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/technexus/ticketpass/TechnexusSpringTicketpassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.technexus.ticketpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnexusSpringTicketpassApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
